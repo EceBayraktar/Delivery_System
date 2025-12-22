49b4f21a1c5da549ae30bf9db5db8559fb35abf0
Sudenaz Arıkan <arikansudenaz20@gmail.com>
2025-12-22 16:04:42 +0300

    Update

---
+++ shipment-service/src/main/java/com/delivery/shipmentservice/model/ShipmentStatus.java
@@ -0,0 +1,9 @@
+package com.delivery.shipmentservice.model;
+
+public enum ShipmentStatus {
+    CREATED,
+    ASSIGNED,
+    IN_TRANSIT,
+    DELIVERED,
+    CANCELLED
+}
--- shipment-service/src/main/java/com/delivery/shipmentservice/service/ShipmentService.java
+++ shipment-service/src/main/java/com/delivery/shipmentservice/service/ShipmentService.java
@@ -2,6 +2,7 @@ package com.delivery.shipmentservice.service;
 
 import com.delivery.shipmentservice.event.ShipmentEventPublisher;
 import com.delivery.shipmentservice.model.Shipment;
+import com.delivery.shipmentservice.model.ShipmentStatus;
 import com.delivery.shipmentservice.repository.ShipmentRepository;
 import org.springframework.stereotype.Service;
 
@@ -23,29 +24,42 @@ public class ShipmentService {
         return repository.findAll();
     }
 
-    public Shipment getById(String id) {
-        return repository.findById(id).orElse(null);
+    public Shipment getById(UUID id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Shipment not found"));
     }
 
-    public Shipment createShipment(Shipment shipment) {
-        shipment.setId(UUID.randomUUID().toString());
-        shipment.setStatus("Created");
-        return repository.save(shipment);
+    public Shipment createShipment(String sender, String receiver) {
+        Shipment shipment = new Shipment(sender, receiver);
+        shipment.setId(UUID.randomUUID());
+
+        Shipment saved = repository.save(shipment);
+
+        // Kafka event (CREATED)
+        publisher.publishStatusChanged(
+                saved.getId().toString(),
+                saved.getStatus().name()
+        );
+
+        return saved;
     }
 
-    public Shipment updateStatus(String id, String status) {
+    public Shipment updateStatus(UUID id, ShipmentStatus status) {
         Shipment shipment = getById(id);
-        if (shipment != null) {
-            shipment.setStatus(status);
-            repository.save(shipment);
+        shipment.setStatus(status);
 
-            // Kafka event gönder
-            publisher.publishStatusChanged(id, status);
-        }
-        return shipment;
+        Shipment updated = repository.save(shipment);
+
+        // Kafka event
+        publisher.publishStatusChanged(
+                id.toString(),
+                status.name()
+        );
+
+        return updated;
     }
 
-    public void deleteShipment(String id) {
+    public void deleteShipment(UUID id) {
         repository.deleteById(id);
     }
 }
